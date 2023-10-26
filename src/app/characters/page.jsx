b99e990db39f926350561fdf88a59cbcafd3c9f7
serrsua <serrsua.88@gmail.com>
2023-10-26 17:42:40 -0300

    llamadas axios

--- src/app/characters/page.jsx
+++ src/app/characters/page.jsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import Principal from "../components/principal";
+import axios from "axios";
+const {API_KEY, HASH } = process.env;
 
-const characters = "axios";
 
-export default function Characters() {
+export default async function Characters() {
+  
+  /* const characters = (await (axios.get(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${API_KEY}&hash=${HASH}`))).data.data; */
+  /* console.log(characters); */
+
   return (
     <main>
       <h1>Personajes de Marvel</h1>
+      <Principal title={"Todos los personajes de Marvel"} data={characters} />
       <Link href={"/"}>Inicio</Link>
     </main>
   );
--- src/app/comics/page.jsx
+++ src/app/comics/page.jsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import Principal from "../components/principal";
+import axios from "axios";
+const {API_KEY, HASH } = process.env;
 
-export default function Comics() {
+export default async function Comics() {
+
+  /* const comics = (await (axios.get(`https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=${API_KEY}&hash=${HASH}`))).data.data; */
+
+  /* console.log(comics); */
+
     return (
       <main>
         <h1>Comics de Marvel</h1>
+        <Principal title={"Todos los comics de Marvel"} data={comics} />
         <Link href={"/"} >Inicio</Link>
       </main>
     )
