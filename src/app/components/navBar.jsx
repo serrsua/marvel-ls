05ad5aa9606f8241a6ea103bc05b5b2ae0806515
Lukkas98 <lucaspalma988@gmail.com>
2023-11-03 17:59:29 -0300

    add navBar and Sidebard

---
+++ src/app/components/navBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = ({ title }) => {
+  return (
+    <nav className=" bg-stone-500 col-span-2 flex justify-between px-10 py-4">
+      <h1>{title}</h1>
+      <input type="text" name="" id="" />
+    </nav>
+  );
+};
+
+export default NavBar;
