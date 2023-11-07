0f2b38664fc7cd9153fa99714a9f4dddfb270e96
serrsua <serrsua.88@gmail.com>
2023-11-07 16:46:01 -0300

    funcion para paginado

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
 
 const Main = ({ data, category }) => {
-  console.log("data: ", data.length);
-  console.log("category: ", category);
 
   return (
     <div className=" flex flex-wrap gap-3" >
       {data.map((obj) => (
         <div key={obj.id} className=" p-2 bg-yellow-400" >
-          <p>{obj.name}</p>
+          <p>{obj.name ?? obj.title}</p>
           <Image src={obj.image} width={150} height={150} alt={`image of ${obj.name}`} />
         </div>
       ))}
