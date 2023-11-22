b5a9bcb62723c0e2ff323ccd045e7b27c1a05bce
Lukkas98 <lucaspalma988@gmail.com>
2023-11-22 19:16:00 -0300

    add detail loading

---
+++ src/app/detail/[id]/loading.jsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className=" bg-slate-400 flex flex-col items-center">
+      <p>CARGANDO...</p>
+      <img src="/assets/loading.gif" alt="un gif" className="w-1/4" />
+    </div>
+  );
+};
+
+export default Loading;
