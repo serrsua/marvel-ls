7f639c9c1a60ebff706afebe50c8095867a0567e
Lukkas98 <lucaspalma988@gmail.com>
2024-03-28 17:00:18 -0300

    estilos

--- src/app/detail/[id]/page.jsx
+++ src/app/detail/[id]/page.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Image from "next/image";
 import Link from "next/link";
 
 const Detail = async ({ params, searchParams }) => {
@@ -14,31 +15,46 @@ const Detail = async ({ params, searchParams }) => {
 
   return (
     <section>
-      <div>
-        <Link href={`/home/category/${category}?offset=${offset}`} >Volver</Link>
-        <h2>{`Nombre: ${name ?? title}`}</h2>
-        <p>{`Descripción: ${description}`}</p>
-        <img
-          src={`${thumbnail.path}.${thumbnail.extension}`}
-          alt="imagen del personaje"
-        />
-        <div>
+      <div className="flex flex-col items-center gap-3 mt-4">
+        <Link
+          href={`/home/category/${category}?offset=${offset}`}
+          className="bg-orange-400 px-2 py-1 rounded-lg"
+        >
+          Volver
+        </Link>
+        <h2 className="text-4xl font-bold">{`${name ?? title}`}</h2>
+        <p className="p-3 text-center bg-orange-300 bg-opacity-40 rounded-2xl">{`${description}`}</p>
+        <div className="relative w-[90%] h-[300px]">
+          <Image
+            src={`${thumbnail.path}.${thumbnail.extension}`}
+            alt="imagen del personaje"
+            fill={true}
+            className="rounded-md"
+          />
+        </div>
+        <div className=" bg-green-300 bg-opacity-40 rounded-2xl p-2">
           <ul>
             <h6>{comics?.items?.length ? "Comics:" : null}</h6>
             {comics?.items?.map((comic, i) => (
-              <li key={i}>{comic.name}</li>
+              <li className="ml-3" key={i}>
+                {comic.name}
+              </li>
             ))}
           </ul>
           <ul>
             <h6>{series?.items?.length ? "Series:" : null}</h6>
             {series?.items?.map((serie, i) => (
-              <li key={i}>{serie.name}</li>
+              <li className="ml-3" key={i}>
+                {serie.name}
+              </li>
             ))}
           </ul>
           <ul>
             <h6>{characters?.items?.length ? "Personajes:" : null}</h6>
             {characters?.items?.map((character, i) => (
-              <li key={i}>{character.name}</li>
+              <li className="ml-3" key={i}>
+                {character.name}
+              </li>
             ))}
           </ul>
         </div>
