7825ead7635cd3d2943b1c9feb7d26a3d27769d2
Lukkas98 <lucaspalma988@gmail.com>
2024-04-03 21:20:15 -0300

    estilos pagina principal

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 
 const data = [
-  { href: "/home/characters", title: "Personajes", img: "/assets/personajes.webp" },
+  {
+    href: "/home/characters",
+    title: "Personajes",
+    img: "/assets/personajes.webp",
+  },
   { href: "/home/comics", title: "Comics", img: "/assets/comics.webp" },
   { href: "/home/series", title: "Series", img: "/assets/series.webp" },
 ];
@@ -10,7 +14,7 @@ const data = [
 export default function Home() {
   return (
     <main className="flex flex-col items-center bg-black bg-opacity-90">
-      <h1 className=" text-4xl font-bold mt-10 text-white">Mundo Marvel</h1>
+      <h1 className=" text-4xl font-bold mt-10 text-teal-50">Mundo Marvel</h1>
       <div className="flex flex-col gap-5 m-10 w-full items-center">
         {data.map((route, i) => (
           <div
@@ -18,12 +22,13 @@ export default function Home() {
             key={i}
           >
             <Link
-              className=" flex justify-center items-center relative rounded-lg bg-red-400 h-4/5 w-4/5"
+              className="flex justify-center items-center relative rounded-2xl overflow-hidden bg-red-400 h-4/5 w-4/5 duration-300 transition-all group hover:opacity-70 "
               href={`${route.href}?offset=0`}
             >
-              <Image sizes="100vw" fill={true} src={route.img} />
-              <h2 className=" absolute bg-white text-black text-4xl" >{route.title}</h2>
-
+              <Image sizes="90vw" fill={true} alt="portada" src={route.img} />
+              <h2 className=" absolute bg-stone-900 text-teal-50 text-4xl font-bold px-2 py-1 bg-opacity-70 rounded-2xl duration-300 transition-all group-hover:text-green-400 group-hover:scale-125 md:group-hover:scale-150">
+                {route.title}
+              </h2>
             </Link>
           </div>
         ))}
